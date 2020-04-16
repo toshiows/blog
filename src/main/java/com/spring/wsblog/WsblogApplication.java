3532bcfec3e6d81bda6d749fc92dd6678e4a095a
Toshio Watanabe <toshio_ws@hotmail.com>
2020-04-15 21:55:06 -0300

    Projeto criado

---
+++ src/main/java/com/spring/wsblog/WsblogApplication.java
@@ -0,0 +1,13 @@
+package com.spring.wsblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsblogApplication.class, args);
+	}
+
+}
