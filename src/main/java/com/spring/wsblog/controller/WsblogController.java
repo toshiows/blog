5fd9cf7d5dd324a03e9d5e3d3768675778451bf4
Toshio Watanabe <toshio_ws@hotmail.com>
2020-04-18 14:38:37 -0300

    Criado o metodo no controller para salvar post

--- src/main/java/com/spring/wsblog/controller/WsblogController.java
+++ src/main/java/com/spring/wsblog/controller/WsblogController.java
@@ -1,13 +1,18 @@
 package com.spring.wsblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.wsblog.model.Post;
 import com.spring.wsblog.service.WsblogService;
@@ -37,4 +42,23 @@ public class WsblogController {
 		
 		return mv;
 	}
+	
+	@RequestMapping(value="/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value="/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		
+		post.setData(LocalDate.now());
+		wsblogService.save(post);
+		return "redirect:/posts";
+	}
+	
+	
+	
 }
